e8d768ae8b6d8f24545ca48c11837e7d939adb2a
Ezequiel Tavares <eot_ccb@hotmail.com>
2025-08-06 22:38:48 -0300

    feat: :art: implements axios api and external request for Dictionary API

--- backend/src/@types/env.d.ts
+++ backend/src/@types/env.d.ts
@@ -3,5 +3,7 @@ declare namespace NodeJS {
         PORT: string;
         JWT_SECRET: string;
         REDIS_URL: string;
+        DATABASE_URL: string;
+        DICTIONARY_URL: string;
     }
 }   
---
+++ backend/src/application/dictionary.service.ts
@@ -0,0 +1,44 @@
+import redisClient from "@/infrastructure/cache/redis.js"
+import api from "@/infrastructure/lib/api.js";
+import { DictionaryResponse } from "@/infrastructure/types/dictionary.js";
+
+export class DictionaryService {
+    private async getWordDictionary(word: string) {
+        try {
+            const { data } = await api.get<DictionaryResponse>(`/${word}`);
+            return data
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    async searchWord(word: string) {
+        const cacheKey = `dictionary:word:${word.toLowerCase()}`
+
+        if (redisClient) {
+            const cached = await redisClient.get(cacheKey)
+
+            if (cached) {
+                return {
+                    word: JSON.parse(cached),
+                    fromCache: true
+                };
+            }
+        }
+
+        const wordDictionary = await this.getWordDictionary(word);
+
+        if (!wordDictionary) {
+            return null;
+        }
+
+        if (redisClient) {
+            await redisClient.set(cacheKey, JSON.stringify(wordDictionary));
+        }
+
+        return {
+            word: wordDictionary,
+            fromCache: false
+        };
+    }
+}
